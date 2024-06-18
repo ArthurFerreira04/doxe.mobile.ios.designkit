8e04ead106a008a7ccb11e59c5a4526373b07d5c
DiLopes <31226819+DiLopes@users.noreply.github.com>
2024-06-18 19:15:30 -0300

    Ajuste layout componente de input

--- DSDoxe/Sources/DSDoxe/Button/ContentView.swift
+++ DSDoxe/Sources/DSDoxe/Button/ContentView.swift
@@ -8,51 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var fieldState: DoxeInputFieldState = .normal
+    @State private var state: DoxeInputFieldState = .normal
 
     var body: some View {
-        VStack(spacing: 20) {
+        VStack {
             DoxeInputField(
-                state: $fieldState,
-                titleLabel: "Label",
-                placeholder: "Place Holder",
-                supportingText: "Texto de apoio",
-                errorMessage: nil,
-                leftIcon: "magnifyingglass",
-                rightIcon: nil
-            )
-            
-            DoxeInputField(
-                state: $fieldState,
-                titleLabel: "Label",
-                placeholder: "Place Holder",
-                supportingText: "Texto de apoio",
-                errorMessage: nil,
-                leftIcon: nil,
-                rightIcon: "magnifyingglass"
-            )
-            
-            DoxeInputField(
-                state: $fieldState,
-                titleLabel: "Label",
-                placeholder: "Place Holder",
+                state: $state,
+                titleLabel: "Digite seu e-mail",
+                placeholder: "email@gmail.com",
                 supportingText: "Texto de apoio",
                 errorMessage: "Mensagem de erro",
                 leftIcon: "magnifyingglass",
                 rightIcon: "magnifyingglass"
             )
-            
-            DoxeInputField(
-                state: $fieldState,
-                titleLabel: "Label",
-                placeholder: "Place Holder",
-                supportingText: "Texto de apoio",
-                errorMessage: nil,
-                leftIcon: nil,
-                rightIcon: nil
-            )
+            .padding()
         }
-        .padding()
     }
 }
 
--- DSDoxe/Sources/DSDoxe/Inputs/DoxeInputField.swift
+++ DSDoxe/Sources/DSDoxe/Inputs/DoxeInputField.swift
@@ -26,64 +26,90 @@ struct DoxeInputField: View {
 
     var body: some View {
         VStack(alignment: .leading, spacing: 4) {
-            HStack {
-                if let leftIcon = leftIcon {
-                    Image(systemName: leftIcon)
-                        .foregroundColor(iconColor)
-                }
-                
-                TextField(placeholder, text: $text, onEditingChanged: { isEditing in
-                    if isEditing {
-                        self.state = .focused
-                    } else if !text.isEmpty {
-                        self.state = .filled
-                    } else {
-                        self.state = .normal
+            ZStack(alignment: .leading) {
+                HStack {
+                    if let leftIcon = leftIcon {
+                        Image(systemName: leftIcon)
+                            .foregroundColor(iconColor)
+                            .padding(.leading, 8)
                     }
-                })
-                .foregroundColor(state == .error ? .red : .primary)
-                
-                if let rightIcon = rightIcon {
-                    Image(systemName: rightIcon)
-                        .foregroundColor(iconColor)
+
+                    VStack(alignment: .leading, spacing: -4) {
+                        Text(titleLabel)
+                            .font(.custom("", size: 12))
+                            .foregroundColor(state == .error ? .red : .black)
+                            .padding(.leading, leftIcon == nil ? 8 : 8)
+                            .background(Color.white.opacity(0.8))
+                            
+
+                        ZStack(alignment: .leading) {
+                            if text.isEmpty {
+                                Text(placeholder)
+                                    .font(.custom("", size: 12)) 
+                                    .foregroundColor(.gray)
+                                    .padding(.leading, leftIcon == nil ? 8 : 8)
+                            }
+                            TextField("", text: $text, onEditingChanged: { isEditing in
+                                if isEditing {
+                                    self.state = .focused
+                                } else if !text.isEmpty {
+                                    self.state = .filled
+                                } else {
+                                    self.state = .normal
+                                }
+                            })
+                            .font(.custom("", size: 12)) 
+                            .padding(EdgeInsets(top: 4, leading: 8, bottom: 8, trailing: 12))
+                            .foregroundColor(state == .error ? .red : .primary)
+                        }
+                    }
+
+                    if let rightIcon = rightIcon {
+                        Image(systemName: rightIcon)
+                            .foregroundColor(iconColor)
+                            .padding(.trailing, 8)
+                    }
                 }
+                .padding(.horizontal, 8)
+                .padding(.vertical, 8)
+                .background(RoundedRectangle(cornerRadius: 12)
+                                .strokeBorder(borderColor, lineWidth: 1)
+                                .background(RoundedRectangle(cornerRadius: 12).fill(backgroundColor)))
             }
-            .padding()
-            .background(RoundedRectangle(cornerRadius: 8)
-                            .strokeBorder(borderColor, lineWidth: 2)
-                            .background(RoundedRectangle(cornerRadius: 8).fill(backgroundColor)))
-            
-            
-            Text(supportingText)
-                .font(.footnote)
-                .foregroundColor(.gray)
-            
+
+            if state != .error {
+                Text(supportingText)
+                    .font(.custom("", size: 12)) 
+                    .foregroundColor(.gray)
+            }
+
             if let errorMessage = errorMessage, state == .error {
                 Text(errorMessage)
-                    .font(.footnote)
+                    .font(.custom("", size: 12))
                     .foregroundColor(.red)
             }
         }
         .padding(.horizontal)
+        .animation(.easeInOut, value: text.isEmpty)
     }
-    
+
     private var borderColor: Color {
         switch state {
         case .normal:
-            return .clear
+            return .gray
         case .filled:
             return .purple
         case .focused:
-            return .gray
+            return .blue
         case .error:
             return .red
         }
     }
-    
+
     private var backgroundColor: Color {
-        return state == .focused ? Color.blue.opacity(0.1) : Color.clear
+        return .white
     }
-    
+
     private var iconColor: Color {
         return state == .error ? .red : .gray
     }
