e8a8bdabb823c48d5bf111561b5ee037b2e4530d
Filipe Wolf Boeck <filipe.boeck@hotmail.com>
2024-10-04 23:17:08 -0300

    update: change to public spacing

--- Sources/DesignKit/Spacing/Spacing.swift
+++ Sources/DesignKit/Spacing/Spacing.swift
@@ -5,29 +5,29 @@ public struct Spacing {
     public static let single: CGFloat = 1
 
     /// Double unit spacing (2 pt)
-    static let double: CGFloat = 2
+    public static let double: CGFloat = 2
 
     /// Tiny spacing, typically used for very small gaps (4 pt)
-    static let tiny: CGFloat = 4
+    public static let tiny: CGFloat = 4
 
     /// Small spacing, useful for small gaps between elements (8 pt)
-    static let small: CGFloat = 8
+    public static let small: CGFloat = 8
 
     /// Small to medium spacing, a bit larger than small but not quite medium (12 pt)
-    static let smallMedium: CGFloat = 12
+    public static let smallMedium: CGFloat = 12
 
     /// Medium spacing, standard spacing for most elements (16 pt)
-    static let medium: CGFloat = 16
+    public static let medium: CGFloat = 16
 
     /// Medium to large spacing, larger than medium for more significant separation (24 pt)
-    static let mediumLarge: CGFloat = 24
+    public static let mediumLarge: CGFloat = 24
 
     /// Large spacing, used for significant gaps or padding (32 pt)
-    static let large: CGFloat = 32
+    public static let large: CGFloat = 32
 
     /// Large to huge spacing, very large gaps, typically for major section breaks (48 pt)
-    static let largeHuge: CGFloat = 48
+    public static let largeHuge: CGFloat = 48
 
     /// Huge spacing, the largest standard spacing, for major layout separations (64 pt)
-    static let huge: CGFloat = 64
+    public static let huge: CGFloat = 64
 }
